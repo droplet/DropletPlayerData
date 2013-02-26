7332334c18f49161ee4df62cf7f45100ef67a12b
Wulfspider <hello@wulfspider.com>
2013-02-26 15:02:29 -0500

    Add license headers and missing files. A little bit of cleanup and formatting.

--- src/main/java/org/spout/droplet/playerdata/DataEventListener.java
+++ src/main/java/org/spout/droplet/playerdata/DataEventListener.java
@@ -1,3 +1,26 @@
+/*
+ * This file is part of DropletPlayerData.
+ *
+ * Copyright (c) 2013 Spout LLC <http://www.spout.org/>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 package org.spout.droplet.playerdata;
 
 import java.io.IOException;
@@ -70,10 +93,10 @@ public class DataEventListener implements Listener {
 		Connection conn = null;
 		try {
 			conn = plugin.getConnectionPool().getConnection();
-			//Try to update the existing row
+			// Try to update the existing row
 			PreparedStatement st = setupUpdateStatement(conn, snapshot, false);
 			if (st.executeUpdate() == 0) {
-				//If no rows were updated, insert a new one
+				// If no rows were updated, insert a new one
 				setupUpdateStatement(conn, snapshot, true).execute();
 			}
 			event.setSaved(true);
